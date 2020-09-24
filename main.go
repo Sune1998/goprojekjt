52e22b477f9c0dc62264c4d72164c951846ac1d3
Patrick <patr6996@edu.easj.dk>
2020-09-24 11:28:42 +0200

    Tilføjet metode patrick() til Sunes projekt main

--- main.go
+++ main.go
@@ -2,21 +2,40 @@ package main
 
 import "fmt"
 
-
-
-func main(){
+func main() {
 	fmt.Println("enter a number in F")
 	var input float64
-	fmt.Scanf("%f" , &input)
+	fmt.Scanf("%f", &input)
 
-	output := (input -32) * 5/9
+	output := (input - 32) * 5 / 9
 
 	fmt.Println(output)
 
-	if output <=10 {
+	if output <= 10 {
 		fmt.Println("That's cold my dude")
 	} else {
 		fmt.Println("very hot")
 	}
-	print("hej")
+	println("hej")
+
+	patrick()
+}
+func patrick() {
+	var input string
+
+	fmt.Println("Hello, World! This is my first GO program")
+	fmt.Print("What's your name? ")
+	fmt.Scanf("%s", &input)
+	output := "Greetings, you are my new friend " + input
+
+	fmt.Println(output)
+
+	var (
+		A = 10
+		B = 17
+		C = 7
+	)
+	fmt.Println("My name is Patrick and i am turning ", A+B, " years old this month")
+	fmt.Println("I miss being ", (A+B)-C, " years old")
+
 }
