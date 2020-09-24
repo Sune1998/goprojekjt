58a93d9eb8723f69abb84d9dbe4bbd48c2459e9c
Patrick <patr6996@edu.easj.dk>
2020-09-24 13:21:08 +0200

    Added method: patrick() proofOfLoop() proofOfArray()
    added notes

--- main.go
+++ main.go
@@ -17,28 +17,51 @@ func main() {
 		fmt.Println("very hot")
 	}
 	println("hej")
-
+	//calling patrick method
 	patrick()
 }
 
-//hej
-// hej sune, hilsen patrick
+//method patrick, sets input type to string
 func patrick() {
 	var input string
-
+	//prints souts
 	fmt.Println("Hello, World! This is my first GO program")
 	fmt.Print("What's your name? ")
+	//starts scanner, takes string
 	fmt.Scanf("%s", &input)
+	//declares text string + input to output
 	output := "Greetings, you are my new friend " + input
-
+	//souts input
 	fmt.Println(output)
 
+	//declaring variables
 	var (
 		A = 10
 		B = 17
 		C = 7
 	)
+	//souting text + variables
 	fmt.Println("My name is Patrick and i am turning ", A+B, " years old this month")
 	fmt.Println("I miss being ", (A+B)-C, " years old")
+	//calls method proofOfLoop
+	proofOfLoop()
+}
 
+//Method with loop, prints "GRUPPE GO-UDVIKLING"
+//calls proofOfArray method 5 times.
+func proofOfLoop() {
+	//loop running 5 times
+	for i := 0; i < 5; i++ {
+		fmt.Print("GRUPPE GO-UDVIKLING\n")
+		//calls method proofOfArray
+		proofOfArray()
+	}
+}
+
+//Method with array, declaring array a,
+//filling it with group names and prints (a)
+func proofOfArray() {
+	//declaring array (a)
+	a := []string{"Bro ", "Kelvin ", "Rasmus ", "Patrick "}
+	fmt.Print(a)
 }
