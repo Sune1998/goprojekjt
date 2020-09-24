e4045b632879f00f8afa85bedc5665424c245e69
Patrick <patr6996@edu.easj.dk>
2020-09-24 13:48:25 +0200

    Merged Patrick Branch into Master

--- okg2/main.go
+++ okg2/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("enter a number in F") 			// dette program konvertere F til C og fortæller dig om det er koldt eller varmt.
+	fmt.Println("enter a number in F")
 	var input float64
 	fmt.Scanf("%f", &input)
 
@@ -17,28 +17,78 @@ func main() {
 		fmt.Println("very hot")
 	}
 	println("hej")
-
+	//calling patrick method
 	patrick()
+	//prints a new line
+	fmt.Print("\n")
+	//calling proofOfIf method
+	proofOfIf()
+	fmt.Print("\n")
+	//declaring construct
+	p := person{name: "Patrick", age: 26}
+	fmt.Print(p)
+
 }
 
-//hej
-// hej sune, hilsen patrick
-func patrick() {        				// denne funktion spørg om dit navn 
+//method patrick, sets input type to string
+func patrick() {
 	var input string
-
+	//prints souts
 	fmt.Println("Hello, World! This is my first GO program")
 	fmt.Print("What's your name? ")
+	//starts scanner, takes string
 	fmt.Scanf("%s", &input)
+	//declares text string + input to output
 	output := "Greetings, you are my new friend " + input
-
+	//souts input
 	fmt.Println(output)
 
+	//declaring variables
 	var (
 		A = 10
 		B = 17
 		C = 7
 	)
+	//souting text + variables
 	fmt.Println("My name is Patrick and i am turning ", A+B, " years old this month")
 	fmt.Println("I miss being ", (A+B)-C, " years old")
+	//calls method proofOfLoop
+	proofOfLoop()
+}
 
+//Method with loop, prints "GRUPPE GO-UDVIKLING"
+//calls proofOfArray method 5 times.
+func proofOfLoop() {
+	//loop running 5 times
+	for i := 0; i < 5; i++ {
+		fmt.Print("GRUPPE GO-UDVIKLING\n")
+		//calls method proofOfArray
+		proofOfArray()
+	}
+}
+
+//Method with array, declaring array a,
+//filling it with group names and prints (a)
+func proofOfArray() {
+	//declaring array (a)
+	a := []string{"Bro ", "Kelvin ", "Rasmus ", "Patrick "}
+	fmt.Print(a)
+}
+
+func proofOfIf() {
+	//delcaring sum og sum1
+	sum := (5 + 5)
+	sum1 := (15 - 5)
+	//if statement
+	if sum == sum1 {
+		fmt.Print("Dette er et if statement")
+	}
+}
+
+//GO way of creating construct
+type person struct {
+	//setting name field to take string
+	//setting age field to take int
+	name string
+	age  int
 }
